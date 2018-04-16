695eec41db91c1a960f80636c57d473d22c4e232
Sangeun44 <eunsang@sas.upenn.edu>
2018-04-16 17:14:33 -0400

    ca

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import {vec3, vec4} from 'gl-matrix';
 import * as Stats from 'stats-js';
 import * as DAT from 'dat-gui';
+import Mesh from './geometry/Mesh';
 
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
 import Cube from './geometry/Cube';
-import Cylinder from './geometry/Cylinder';
-import Flower from './geometry/flower';
-import Tree from './geometry/Tree';
-import Base from './geometry/Base';
 
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
@@ -16,19 +13,10 @@ import {setGL} from './globals';
 import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 //import * as fs from 'fs';
 
-//l-sys
-import Lsystem from './lsystem';
-
-//turtle
-import Turtle from './turtle';
 
 // Define an object with application parameters and button callbacks
 // This will be referred to by dat.GUI's functions that add GUI elements.
 const controls = {
-  shaders: 'lambert',
-  shape: 'coral',
-  color: [255, 0, 105, 1.0], // CSS string
-  iterations: 4,
   randomize: 2,
   'Load Scene': loadScene // A function pointer, essentially
 };
@@ -36,12 +24,7 @@ const controls = {
 //shapes
 let icosphere: Icosphere;
 let square: Square;
-let cylinder: Cylinder;
 let cube: Cube;
-let flower: Flower;
-let tree1: Tree;
-let tree2: Tree;
-let base: Base;
 
 let iteration: number;
 let axiom: string;
@@ -50,32 +33,11 @@ let height: number;
 let count: number = 0.0;
 
 function loadScene() {
-  cylinder = new Cylinder(vec3.fromValues(0,0,0));
-  cylinder.create();
-  flower = new Flower(vec3.fromValues(0,0,0));
-  flower.create();
-  base = new Base(vec3.fromValues(0,0,0));
-  base.create();
   square = new Square(vec3.fromValues(0,0,0));
   square.create();
-  tree1.create();
-  tree2.create();
 }
 
 function main() {
-    //lsystem
-    axiom = "FFFFFFFFFFX";
-    height = controls.randomize;
-    iteration = controls.iterations;
-    var lsys = new Lsystem(axiom, iteration);
-    var path = lsys.createPath(); //create string path
-
-    tree1 = new Tree(vec3.fromValues(0,0,0));
-    tree2 = new Tree(vec3.fromValues(0,0,0));
-     //turle action
-    var turtle = new Turtle(tree1, tree2, path, height);
-    turtle.draw();
-
     
   // Initial display for framerate
   const stats = Stats();
@@ -113,93 +75,29 @@ function main() {
   renderer.setClearColor(0.3, 0.7, 0.9, 1);
   gl.enable(gl.DEPTH_TEST);
 
-  const vertex = new ShaderProgram([
-    new Shader(gl.VERTEX_SHADER, require('./shaders/vertex-vert.glsl')),
-    new Shader(gl.FRAGMENT_SHADER, require('./shaders/vertex-frag.glsl')),
-  ]);
-
-  const tree_lambert = new ShaderProgram([
-    new Shader(gl.VERTEX_SHADER, require('./shaders/lambert-vert.glsl')),
-    new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
-  ]);
 
   const base_lambert = new ShaderProgram([
     new Shader(gl.VERTEX_SHADER, require('./shaders/base-lambert-vert.glsl')),
     new Shader(gl.FRAGMENT_SHADER, require('./shaders/base-lambert-frag.glsl')),
   ]);
-
-  const water_shader = new ShaderProgram([
-    new Shader(gl.VERTEX_SHADER, require('./shaders/water-shader-vert.glsl')),
-    new Shader(gl.FRAGMENT_SHADER, require('./shaders/water-shader-frag.glsl')),
-  ]);
-
-  var it = controls.iterations;
-  var ta = controls.randomize;
-  console.log("height: " + ta);
-
+ 
   // This function will be called every frame
   function tick() {
     
     //U_tIME
     count++;
-    vertex.setTime(count)
     
-    let new_color = vec4.fromValues(controls.color[0]/255, controls.color[1]/255, controls.color[2]/255, 1);
     let base_color = vec4.fromValues(64/255, 255/255, 255/255, 1);
-      tree_lambert.setGeometryColor(new_color);  
-      base_lambert.setGeometryColor(base_color);
       camera.update();
       stats.begin();
 
       var height = controls.randomize;
-      if(ta !== height) {
-        var lsys = new Lsystem(axiom, iteration);
-        var path = lsys.createPath(); //create string path
-
-        tree1 = new Tree(vec3.fromValues(0,0,0));
-        tree2 = new Tree(vec3.fromValues(0,0,0));
-
-        //turle action
-       var turtle = new Turtle(tree1, tree2, path, height);
-       turtle.draw();
-   
-       tree1.create();
-       tree2.create();
-
-       ta = height;
-       console.log("height: " + ta);
-      }
-
-      //change in tree
-      iteration = controls.iterations;
-      if(it !== iteration){
-       //lsystem
-        var lsys = new Lsystem(axiom, iteration);
-        var path = lsys.createPath(); //create string path
-        console
-        tree1 = new Tree(vec3.fromValues(0,0,0));
-        tree2 = new Tree(vec3.fromValues(0,0,0));
-
-        //turle action
-        var turtle = new Turtle(tree1, tree2, path, height);
-        turtle.draw();
-        
-        tree1.create();
-        tree2.create();
-      }
-      it = iteration;
 
       gl.viewport(0, 0, window.innerWidth, window.innerHeight);
       renderer.clear();
 
-      //renderer.render(camera, tree_lambert, [flower]);
-      renderer.render(camera, tree_lambert, [tree1]);
-      renderer.render(camera, vertex, [tree2]);
-      renderer.render(camera, tree_lambert, [base]);
       base_lambert.setGeometryColor(base_color);
-      //renderer.render(camera, base_lambert, [square]);
-      //tester cylinder
-      //renderer.render(camera, tree_lambert, [flower]);
+      renderer.render(camera, base_lambert, [square]);
 
     stats.end();
 
---
+++ src/musicAnalyzer.ts
@@ -0,0 +1,189 @@
+import {vec3, vec4} from 'gl-matrix';
+import { analyze } from 'web-audio-beat-detector';
+
+function init(callback, update) {
+
+  var framework = {
+    paused: false,
+    audioStartOffset: 0,
+    audioStartTime: 0,
+    audioBuffer: undefined,
+    cameraPaused: false,
+    automaticSwitchingOn: true
+  };
+
+  function createAndConnectAudioBuffer() {
+    // create the source buffer
+    framework.audioSourceBuffer = framework.audioContext.createBufferSource();
+    // connect source and analyser
+    framework.audioSourceBuffer.connect(framework.audioAnalyser);
+    framework.audioAnalyser.connect(framework.audioContext.destination);
+  }
+
+  function playAudio(file) {
+    createAndConnectAudioBuffer();
+    framework.audioFile = file;
+
+    var fileName = framework.audioFile.name;
+    document.getElementById('guide').innerHTML = "Playing " + fileName;
+    var fileReader = new FileReader();
+    
+    fileReader.onload = function (e) {
+        var fileResult = fileReader.result;
+        framework.audioContext.decodeAudioData(fileResult, function(buffer) {
+          framework.audioSourceBuffer.buffer = buffer;
+          framework.audioBuffer = buffer;
+          framework.audioSourceBuffer.start();
+          framework.audioSourceBuffer.loop = true;
+          analyze(framework.audioSourceBuffer.buffer).then((bpm) => {
+              // the bpm could be analyzed 
+              framework.songBPM = bpm;
+          })
+          .catch((err) => {
+              // something went wrong 
+              console.log("couldn't detect BPM");
+          });
+        }, function(e){"Error with decoding audio data" + e.err});
+    };
+    fileReader.readAsArrayBuffer(framework.audioFile);
+  }
+
+  // run this function after the window loads
+  window.addEventListener('load', function() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x020202, 0);
+
+    document.body.appendChild(renderer.domElement);
+
+    // resize the canvas when the window changes
+    window.addEventListener('resize', function() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    // set up audio processing
+    framework.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    // create analyser
+    framework.audioAnalyser = framework.audioContext.createAnalyser();
+    framework.audioAnalyser.smoothingTimeConstant = 0.3;
+    framework.audioAnalyser.fftSize = 1024;
+    // create the source buffer
+    framework.audioSourceBuffer = framework.audioContext.createBufferSource();
+
+    // connect source and analyser
+    framework.audioSourceBuffer.connect(framework.audioAnalyser);
+    framework.audioAnalyser.connect(framework.audioContext.destination);
+
+    // add drag and drop functionality for uploading audio file
+    window.addEventListener("dragenter", dragenter, false);  
+    window.addEventListener("dragover", dragover, false);
+    window.addEventListener("drop", drop, false);
+    // add pausing functionality via spacebar
+    window.addEventListener("keypress", keypress, false);
+
+    function dragenter(e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+
+    function dragover(e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+
+    function drop(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      if (framework.audioFile == undefined) {
+        playAudio(e.dataTransfer.files[0]);
+      } else {
+        // stop current visualization and load new song
+        framework.audioSourceBuffer.stop();
+        playAudio(e.dataTransfer.files[0]);
+      }
+    }
+
+    function keypress(e) {
+      // spacebar
+      if (e.keyCode == 32 && framework.audioBuffer != undefined) {
+        if (!framework.paused) {
+          framework.paused = true;
+          framework.audioSourceBuffer.stop();
+          // Measure how much time passed since the last pause.
+          framework.audioStartOffset += framework.audioContext.currentTime - framework.audioStartTime;
+        } else {
+          framework.paused = false;
+          framework.audioStartTime = framework.audioContext.currentTime;
+
+          createAndConnectAudioBuffer();
+          framework.audioSourceBuffer.buffer = framework.audioBuffer;
+          // Start playback, but make sure we stay in bound of the buffer.
+          framework.audioSourceBuffer.start(0, framework.audioStartOffset % framework.audioBuffer.duration);
+          framework.audioSourceBuffer.loop = true;
+        }
+      }
+      // 0
+      if (e.keyCode == 48) {
+        framework.visualizerIndex = 0;
+      }
+      // 1
+      if (e.keyCode == 49) {
+        framework.visualizerIndex = 1;
+      }
+      // 2
+      if (e.keyCode == 50) {
+        framework.visualizerIndex = 2;
+      }
+      // 3
+      if (e.keyCode == 51) {
+        framework.visualizerIndex = 3;
+      }
+      // p
+      if (e.keyCode == 112) {
+        if (!framework.cameraPaused) {
+          framework.cameraPaused = true; 
+        } else {
+          framework.cameraPaused = false;
+        }
+      }
+      // s
+      if (e.keyCode == 115) {
+        if (!framework.automaticSwitchingOn) {
+          framework.automaticSwitchingOn = true; 
+        } else {
+          framework.automaticSwitchingOn = false; 
+        }
+      }
+      // t
+      if (e.keyCode == 116) {
+        // toggle instructions
+        if (document.getElementById('visualizerInfo').style.visibility == "hidden") {
+          document.getElementById('visualizerInfo').style.visibility = "visible";
+        } else {
+          document.getElementById('visualizerInfo').style.visibility = "hidden";
+        }
+      }
+    }
+
+    // assign THREE.js objects to the object we will return
+    framework.scene = scene;
+    framework.camera = camera;
+    framework.renderer = renderer;
+
+    // begin the animation loop
+    (function tick() {
+      update(framework); // perform any requested updates
+      renderer.render(framework.scene, framework.camera); // render the scene
+      requestAnimationFrame(tick); // register to call this again when the browser renders a new frame
+    })();
+
+    // we will pass the scene, gui, renderer, camera, etc... to the callback function
+    return callback(framework);
+  });
+
+}
